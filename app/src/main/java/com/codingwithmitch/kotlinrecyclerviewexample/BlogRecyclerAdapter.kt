977536e54bec45287b5d725b3e4d90ec96c78190
Mitch Tabian <mitch@tabian.ca>
2019-08-26 09:31:06 -0700

    Create BlogRecyclerAdapter

---
+++ app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/BlogRecyclerAdapter.kt
@@ -0,0 +1,66 @@
+package com.codingwithmitch.kotlinrecyclerviewexample
+
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_blog_list_item.view.*
+import com.bumptech.glide.Glide
+import com.codingwithmitch.kotlinrecyclerviewexample.models.BlogPost
+import kotlin.collections.ArrayList
+
+
+class BlogRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+
+    private val TAG: String = "AppDebug"
+
+    private var items: List<BlogPost> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BlogViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_blog_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+
+            is BlogViewHolder -> {
+                holder.bind(items.get(position))
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(blogList: List<BlogPost>){
+        items = blogList
+    }
+
+    class BlogViewHolder
+    constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+
+        val blog_image = itemView.blog_image
+        val blog_title = itemView.blog_title
+        val blog_author = itemView.blog_author
+
+        fun bind(blogPost: BlogPost){
+            Glide.with(itemView.context)
+                .load(blogPost.image)
+                .into(blog_image)
+            blog_title.setText(blogPost.title)
+            blog_author.setText(blogPost.username)
+
+        }
+
+    }
+
+}
