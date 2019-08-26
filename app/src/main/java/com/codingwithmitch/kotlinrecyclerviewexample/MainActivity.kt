f72b1e453a49affb9024132d8a6b1e078bb43477
Mitch Tabian <mitch@tabian.ca>
2019-08-26 10:03:40 -0700

    read json data and re-add images (fix mistake)

--- app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
@@ -2,17 +2,32 @@ package com.codingwithmitch.kotlinrecyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getDataFromAssets()
     }
 
 
-    fun getData(){
-
+    fun getDataFromAssets(): String?{
+        var json: String? = null
+        try{
+            val inputStream: InputStream = assets.open("data.json")
+            val buffer = ByteArray(inputStream.available())
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer)
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        println("data.json: ${json}")
+        return json
     }
 }
 
