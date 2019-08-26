46f3520ee7f7fb9f5889820016b253ecd2dfa244
Mitch Tabian <mitch@tabian.ca>
2019-08-26 08:48:31 -0700

    Revert "testing first commit"
    This reverts commit 98024f89cdee566a6bf35784f99772e882b539ae.

--- app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
@@ -8,7 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Testing first commit
     }
 }
