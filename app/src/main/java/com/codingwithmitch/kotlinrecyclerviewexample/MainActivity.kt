1842d2411f681b6eb1ef4a5053d87f8148d273db
Mitch Tabian <mitch@tabian.ca>
2019-08-26 13:34:19 -0700

    add DataSource.kt

--- app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/kotlinrecyclerviewexample/MainActivity.kt
@@ -14,40 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = convertJsonToBlogPosts(JSONArray(getDataFromAssets()))
     }
 
 
 
-    fun convertJsonToBlogPosts(jsonArray: JSONArray): ArrayList<BlogPost>{
-        val list = ArrayList<BlogPost>()
-        for(i in 0 until jsonArray.length()){
-            list.add(
-                BlogPost(
-                    (jsonArray[i] as JSONObject)["title"] as String,
-                    (jsonArray[i] as JSONObject)["body"] as String,
-                    (jsonArray[i] as JSONObject)["image"] as String,
-                    (jsonArray[i] as JSONObject)["username"] as String
-                    )
-            )
-        }
-        return list
-    }
-
-    fun getDataFromAssets(): String?{
-        var json: String? = null
-        try{
-            val inputStream: InputStream = assets.open("data.json")
-            val buffer = ByteArray(inputStream.available())
-            inputStream.read(buffer)
-            inputStream.close()
-            json = String(buffer)
-        }catch (e: IOException){
-            e.printStackTrace()
-        }
-//        println("data.json: ${json}")
-        return json
-    }
 }
 
 
